5a9b0eae8d9e7519abe1456bae639e289479e380
zefrost77 <alexis.fauxbaton@gmail.com>
2022-10-16 21:21:14 +0200

    Prediction script + triple barrier labeling

--- data_processing.py
+++ data_processing.py
@@ -6,50 +6,64 @@ from datetime import datetime
 from datetime import timedelta
 import requests
 
-def add_adx(df: pd.DataFrame(), interval: int=14):
-  df['-DM'] = df['Low'].shift(1) - df['Low']
-  df['+DM'] = df['High'] - df['High'].shift(1)
-  df['+DM'] = np.where((df['+DM'] > df['-DM']) & (df['+DM']>0), df['+DM'], 0.0)
-  df['-DM'] = np.where((df['-DM'] > df['+DM']) & (df['-DM']>0), df['-DM'], 0.0)
-  df['TR_TMP1'] = df['High'] - df['Low']
-  if "Adj Close" in df.columns:
-    df['TR_TMP2'] = np.abs(df['High'] - df['Adj Close'].shift(1))
-    df['TR_TMP3'] = np.abs(df['Low'] - df['Adj Close'].shift(1))
-  else:
-    df['TR_TMP2'] = np.abs(df['High'] - df['Close'].shift(1))
-    df['TR_TMP3'] = np.abs(df['Low'] - df['Close'].shift(1))
-  df['TR'] = df[['TR_TMP1', 'TR_TMP2', 'TR_TMP3']].max(axis=1)
-  df['TR'+str(interval)] = df['TR'].rolling(interval).sum()
-  df['+DMI'+str(interval)] = df['+DM'].rolling(interval).sum()
-  df['-DMI'+str(interval)] = df['-DM'].rolling(interval).sum()
-  df['+DI'+str(interval)] = df['+DMI'+str(interval)] /   df['TR'+str(interval)]*100
-  df['-DI'+str(interval)] = df['-DMI'+str(interval)] / df['TR'+str(interval)]*100
-  df['DI'+str(interval)+'-'] = abs(df['+DI'+str(interval)] - df['-DI'+str(interval)])
-  df['DI'+str(interval)] = df['+DI'+str(interval)] + df['-DI'+str(interval)]
-  df['DX'] = (df['DI'+str(interval)+'-'] / df['DI'+str(interval)])*100
-  df['ADX'+str(interval)] = df['DX'].rolling(interval).mean()
-  df['ADX'+str(interval)] =   df['ADX'+str(interval)].fillna(df['ADX'+str(interval)].mean())
-  del df['TR_TMP1'], df['TR_TMP2'], df['TR_TMP3'], df['TR'], df['TR'+str(interval)]
-  del df['+DMI'+str(interval)], df['DI'+str(interval)+'-']
-  del df['DI'+str(interval)], df['-DMI'+str(interval)]
-  del df['+DI'+str(interval)], df['-DI'+str(interval)]
-  del df['DX']
-  return df
+# Only processes 1m data for now
+
+
+def add_adx(df: pd.DataFrame(), interval: int = 14):
+    df['-DM'] = df['Low'].shift(1) - df['Low']
+    df['+DM'] = df['High'] - df['High'].shift(1)
+    df['+DM'] = np.where((df['+DM'] > df['-DM']) &
+                         (df['+DM'] > 0), df['+DM'], 0.0)
+    df['-DM'] = np.where((df['-DM'] > df['+DM']) &
+                         (df['-DM'] > 0), df['-DM'], 0.0)
+    df['TR_TMP1'] = df['High'] - df['Low']
+    if "Adj Close" in df.columns:
+        df['TR_TMP2'] = np.abs(df['High'] - df['Adj Close'].shift(1))
+        df['TR_TMP3'] = np.abs(df['Low'] - df['Adj Close'].shift(1))
+    else:
+        df['TR_TMP2'] = np.abs(df['High'] - df['Close'].shift(1))
+        df['TR_TMP3'] = np.abs(df['Low'] - df['Close'].shift(1))
+    df['TR'] = df[['TR_TMP1', 'TR_TMP2', 'TR_TMP3']].max(axis=1)
+    df['TR'+str(interval)] = df['TR'].rolling(interval).sum()
+    df['+DMI'+str(interval)] = df['+DM'].rolling(interval).sum()
+    df['-DMI'+str(interval)] = df['-DM'].rolling(interval).sum()
+    df['+DI'+str(interval)] = df['+DMI'+str(interval)] / \
+        df['TR'+str(interval)]*100
+    df['-DI'+str(interval)] = df['-DMI'+str(interval)] / \
+        df['TR'+str(interval)]*100
+    df['DI'+str(interval)+'-'] = abs(df['+DI'+str(interval)] -
+                                     df['-DI'+str(interval)])
+    df['DI'+str(interval)] = df['+DI'+str(interval)] + df['-DI'+str(interval)]
+    df['DX'] = (df['DI'+str(interval)+'-'] / df['DI'+str(interval)])*100
+    df['ADX'+str(interval)] = df['DX'].rolling(interval).mean()
+    df['ADX'+str(interval)] = df['ADX'+str(interval)
+                                 ].fillna(df['ADX'+str(interval)].mean())
+    del df['TR_TMP1'], df['TR_TMP2'], df['TR_TMP3'], df['TR'], df['TR' +
+                                                                  str(interval)]
+    del df['+DMI'+str(interval)], df['DI'+str(interval)+'-']
+    del df['DI'+str(interval)], df['-DMI'+str(interval)]
+    del df['+DI'+str(interval)], df['-DI'+str(interval)]
+    del df['DX']
+    return df
+
 
 def fear_and_greed():
 
     URL = "https://api.alternative.me/fng/?limit=0"
-    r = requests.get(url = URL)
+    r = requests.get(url=URL)
     fng = r.json()
     fng = pd.DataFrame(fng['data'])
     fng["timestamp"] = fng["timestamp"].astype(int)
     fng["value"] = fng["value"].astype(int)
-    for index,row in fng.iterrows():
-        fng.loc[index,"timestamp"] = datetime.utcfromtimestamp(fng.loc[index,"timestamp"])
-        fng.loc[index,"timestamp"] = fng.loc[index,"timestamp"].strftime("%Y-%m-%d")
-    fng.rename(columns={"timestamp":"Date", "value":"FnG"},inplace=True)
+    for index, row in fng.iterrows():
+        fng.loc[index, "timestamp"] = datetime.utcfromtimestamp(
+            fng.loc[index, "timestamp"])
+        fng.loc[index, "timestamp"] = fng.loc[index,
+                                              "timestamp"].strftime("%Y-%m-%d")
+    fng.rename(columns={"timestamp": "Date", "value": "FnG"}, inplace=True)
 
-    return fng.drop(["time_until_update","value_classification"],axis=1)
+    return fng.drop(["time_until_update", "value_classification"], axis=1)
+
 
 def add_rsi(data):
     df = data.copy()
@@ -66,21 +80,22 @@ def add_rsi(data):
 
     df['RSI'] = 0
 
-    #df['RSI'] = 100 - (100 / (1 + ))
-    
+    # df['RSI'] = 100 - (100 / (1 + ))
+
     df['RSI'] = 100 - (100 / (1 + df['Avg Gain'] / df['Avg Loss']))
 
-    df = df.drop(["Gains", "Losses", "Avg Gain", "Avg Loss"],axis=1)
+    df = df.drop(["Gains", "Losses", "Avg Gain", "Avg Loss"], axis=1)
 
     #df['RSI'].loc[0:14] = 100 - (100 / (1 + df['Avg Gain'].loc[0:14] / df['Avg Loss'].loc[0:14]))
 
-
     return df
+
 
 def add_macd(data):
     df = data.copy()
 
-    df['MACD'] = df["Close"].ewm(span=12, adjust=False).mean() - df["Close"].ewm(span=26, adjust=False).mean()
+    df['MACD'] = df["Close"].ewm(span=12, adjust=False).mean(
+    ) - df["Close"].ewm(span=26, adjust=False).mean()
 
     df['MACD_SIGNAL'] = df["MACD"].ewm(span=9, adjust=False).mean()
 
@@ -90,24 +105,97 @@ def add_macd(data):
 
     return df
 
+
 def confirmation_time(data):
-    df = pd.DataFrame({"Date":[],"Confirmation Time":[]})
+    df = pd.DataFrame({"Date": [], "Confirmation Time": []})
     for i in range(len(data)):
         for j in range(3):
-            date = datetime.strptime(data.loc[i,"Timestamp"][:-9],"%Y-%m-%d")
+            date = datetime.strptime(data.loc[i, "Timestamp"][:-9], "%Y-%m-%d")
             date = date + timedelta(days=j)
-            #print(str(date)[:-9])
-            df = df.append({"Date":str(date)[:-9],"Confirmation Time":data.loc[i,"median-confirmation-time"]},ignore_index=True)
+            # print(str(date)[:-9])
+            df = df.append({"Date": str(date)[
+                           :-9], "Confirmation Time": data.loc[i, "median-confirmation-time"]}, ignore_index=True)
     return df
+
 
 def network_transactions(data):
-    df = pd.DataFrame({"Date":[], "Transactions":[]})
+    df = pd.DataFrame({"Date": [], "Transactions": []})
 
     for i in range(len(data)):
         for j in range(3):
-            date = datetime.strptime(data.loc[i,"Timestamp"][:-9],"%Y-%m-%d")
+            date = datetime.strptime(data.loc[i, "Timestamp"][:-9], "%Y-%m-%d")
             date = date + timedelta(days=j)
-            #print(str(date)[:-9])
-            df = df.append({"Date":str(date)[:-9],"Transactions":data.loc[i,"n-transactions"]},ignore_index=True)
+            # print(str(date)[:-9])
+            df = df.append({"Date": str(date)[
+                           :-9], "Transactions": data.loc[i, "n-transactions"]}, ignore_index=True)
     return df
 
+
+def miners_revenue(data):
+    df = pd.DataFrame({"Date": [], "Miners Revenue": []})
+
+    for i in range(len(data)):
+        for j in range(3):
+            date = datetime.strptime(data.loc[i, "Timestamp"][:-9], "%Y-%m-%d")
+            date = date + timedelta(days=j)
+            df = df.append({"Date": str(date)[
+                           :-9], "Miners Revenue": data.loc[i, "miners-revenue"]}, ignore_index=True)
+
+    return df
+
+
+def standardize_col(data, col):
+    df = data.copy()
+    df[col] = (df[col] - df[col].mean()) / df[col].std()
+    return df
+
+
+def merge_1m():
+    data2017 = pd.read_csv("minute_data/BTC-2017min.csv")
+    data2018 = pd.read_csv("minute_data/BTC-2018min.csv")
+    data2019 = pd.read_csv("minute_data/BTC-2019min.csv")
+    data2020 = pd.read_csv("minute_data/BTC-2020min.csv")
+    data2021 = pd.read_csv("minute_data/BTC-2021min.csv")
+
+    data_by_minute = pd.concat(
+        [data2017, data2018, data2019, data2020, data2021], axis=0)
+    # data_by_minute = data_by_minute.sort_values(by="Unix", ascending=True, inplace=False) # ??????????????????!?!¿!?¿!?¿!?¿!?¿!?!¿?!¿?!¿?¿!?¿!?¿!?¿
+    data_by_minute.to_csv("minute_data/BTC-USD_1m.csv", index=False)
+
+# 1 minute data processing
+
+
+def process_minute_data():
+
+    data = pd.read_csv("minute_data/BTC-USD_1m.csv")
+    data["Variation"] = (
+        data["Close"] - data["Close"].shift(1)) / data["Close"].shift(1)
+    data = add_rsi(data)
+    data = add_macd(data)
+    data = add_adx(data)
+    #data.drop("Adj Close",axis=1,inplace=True)
+    data = standardize_col(data, "Volume USD")
+    data = standardize_col(data, "MACD")
+    data = standardize_col(data, "MACD_H")
+    data = standardize_col(data, "RSI")
+    data = standardize_col(data, "Variation")
+    data = standardize_col(data, ["ADX14", "-DM", "+DM"])
+    '''
+    confirmation = confirmation_time(pd.read_csv("median-confirmation-time.csv"))
+    transactions = network_transactions(pd.read_csv("n-transactions.csv"))
+    miners_revenue = miners_revenue(pd.read_csv("miners-revenue.csv"))
+    fear_and_greed = fear_and_greed()
+    data = pd.merge(data,confirmation,on="Date")
+    data = pd.merge(data,transactions,on="Date")
+    data = pd.merge(data,miners_revenue,on="Date")
+    data = pd.merge(data,fear_and_greed,on="Date")
+    data = standardize_col(data,"Confirmation Time")
+    data = standardize_col(data,"Transactions")
+    data = standardize_col(data,"Miners Revenue")
+    data = standardize_col(data,"FnG")
+    '''
+    data.drop(["Volume BTC"], axis=1, inplace=True)
+    data.dropna(inplace=True)
+    data.sort_values(by="Unix", ascending=True, inplace=True)
+
+    data.to_csv("minute_data/BTC-USD_1M_SIGNALS.csv", index=False)
---
+++ predict.py
@@ -0,0 +1,236 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from datetime import datetime
+from datetime import timedelta
+
+def standard_labeling(data, max_days, target_range):
+    predict_data = data.copy().drop(["Open", "Close", "High", "Low", "Symbol"], axis=1)
+    for i in range(1, max_days):  # 2jours
+        #predict_data[["Variation-{}".format(i),"Vol-{}".format(i),"RSI-{}".format(i),"MACD-{}".format(i),"MACD_H-{}".format(i),"CONF-{}".format(i),"TRANS-{}".format(i),"REV-{}".format(i),"FnG-{}".format(i)]] = data[["Variation","Volume","RSI","MACD","MACD_H","Confirmation Time","Transactions","Miners Revenue","FnG"]].shift(i)
+        #predict_data[["Variation-{}".format(i),"Vol-{}".format(i),"RSI-{}".format(i),"MACD-{}".format(i),"MACD_H-{}".format(i),"CONF-{}".format(i),"TRANS-{}".format(i),"REV-{}".format(i),"FnG-{}".format(i), "ADX-{}".format(i), "+DM-{}".format(i), "-DM-{}".format(i)]] = data[["Variation","Volume","RSI","MACD","MACD_H","Confirmation Time","Transactions","Miners Revenue","FnG","ADX14","+DM","-DM"]].shift(i)
+        predict_data[["Variation-{}".format(i), "RSI-{}".format(i), "MACD-{}".format(
+            i), "MACD_H-{}".format(i)]] = data[["Variation", "RSI", "MACD", "MACD_H"]].shift(i)
+    #predict_data["Target"] = (data["Variation"].shift(-1) >= 0)
+    predict_data["Target"] = (
+        data["Close"].shift(-target_range) - data["Close"] >= 0)
+    predict_data["Target"] = np.where(predict_data["Target"] == True, 1, 0)
+    predict_data.dropna(inplace=True)
+    predict_data.reset_index(inplace=True, drop=True)
+    predict_data = predict_data[0:len(predict_data)-target_range]
+
+    return predict_data
+
+
+def meta_labeling(data, max_days, target_range):
+    predict_data = data.copy().drop(["Open", "Close", "High", "Low", "Symbol"], axis=1)
+    size = data.shape[0]
+    first = None
+    for i in range(1, max_days):  # 2jours
+        predict_data[["Variation-{}".format(i), "RSI-{}".format(i), "MACD-{}".format(
+            i), "MACD_H-{}".format(i)]] = data[["Variation", "RSI", "MACD", "MACD_H"]].shift(i)
+
+    for index,row in data.iterrows():
+        #index = row[0]
+        slice_data = data[index:index+target_range+1]
+        #max_range = data[["Unix","High"]].loc[data["High"] == data["High"][index:index+max_days-1].max()].reset_index(drop=True)
+        #min_range = data[["Unix","Low"]].loc[data["Low"] == data["Low"][index:index+max_days-1].min()].reset_index(drop=True)
+        max_unix = None
+        min_unix = None
+
+        max_range = slice_data[["Unix", "High"]].loc[slice_data["High"]
+                                                     == slice_data["High"].max()].reset_index(drop=True)
+        min_range = slice_data[["Unix", "Low"]].loc[slice_data["Low"]
+                                                    == slice_data["Low"].min()].reset_index(drop=True)
+
+        if (max_range["High"].loc[0]/slice_data["Close"].loc[index]) > 1.005:
+            max_unix = max_range["Unix"].loc[0]
+        if (min_range["Low"].loc[0]/slice_data["Close"].loc[index]) < 0.995:
+            min_unix = min_range["Unix"].loc[0]
+
+        # Encoding max/min state to easily code below which one was encountered first  
+        state_max = 0
+        state_min = 0
+        if max_unix != None:
+            state_max = 1
+        if min_unix != None:
+            state_min = 1
+            
+        state = 2*state_max + state_min
+
+        if state == 0:
+            first = 0
+        elif state == 1:
+            first = -1
+        elif state == 2:
+            first = 1
+        else:
+            if min_unix < max_unix:
+                first = -1
+            else:
+                first = 1            
+
+        predict_data.at[index, "Target"] = first
+
+        if index % 1000 == 0:
+            print("Itération {}/{}".format(index, size), end='\r')
+
+    predict_data["Target1"] = (
+        data["Close"].shift(-target_range) - data["Close"] >= 0)
+    predict_data["Target1"] = np.where(predict_data["Target1"] == True, 1, 0)
+    predict_data.dropna(inplace=True)
+    predict_data.reset_index(inplace=True, drop=True)
+    predict_data = predict_data[0:len(predict_data)-target_range]
+
+    return predict_data
+
+
+def create_predict_data(data, max_days=30, target_range=10, standard=True):
+    read = True
+    if standard:
+        read = False
+        predict_data = standard_labeling(data, max_days, target_range)
+    else:
+        try:
+            
+            predict_data = pd.read_csv("processed_data/processed_1m_{}_{}.csv".format(max_days,target_range))
+            predict_data.drop("Target1", axis=1, inplace=True)
+                        
+            if -1 in predict_data["Target"].values:
+                predict_data["Target"] = predict_data["Target"].replace(1, 2)
+                predict_data["Target"] = predict_data["Target"].replace(0, 1)
+                predict_data["Target"] = predict_data["Target"].replace(-1, 0)
+        except:
+            read = False
+            predict_data = meta_labeling(data, max_days, target_range)
+            if -1 in predict_data["Target"].values:
+                predict_data["Target"] = predict_data["Target"].replace(1, 2)
+                predict_data["Target"] = predict_data["Target"].replace(0, 1)
+                predict_data["Target"] = predict_data["Target"].replace(-1, 0)
+
+    if not read:
+        predict_data = predict_data[[
+            i % int(max_days) == 0 for i in range(len(predict_data))]]
+            
+
+    if (not standard) and (read==False):
+            predict_data.to_csv("processed_data/processed_1m_{}_{}.csv".format(max_days,target_range), index=False)
+
+    return predict_data
+
+
+def yearly_custom_splitter(data1, data2):
+    train_data = data1.drop(["Date", "Unix", "Target"], axis=1)
+    train_labels = data1["Target"]
+    test_data = data2.drop(["Date", "Unix", "Target"], axis=1)
+    test_labels = data2["Target"]
+
+    return train_data, train_labels, test_data, test_labels
+
+
+def sample_equal_target(data):
+    classes = data["Target"].nunique()
+    counts = data["Target"].value_counts().sort_index()
+    
+    #print(counts)
+    
+    #print("Before",data["Target"].value_counts())
+    
+    data["Weights"] = 0
+    for i in range(classes):
+        data["Weights"].loc[data["Target"] == i] = 1/(classes*counts[i])
+        
+    #print(data["Weights"].loc[data["Target"] == 0])
+        
+    sample = data.sample(int(len(data)/2.5),weights=data["Weights"])
+    sample_labels = sample["Target"]
+    sample.drop("Weights", inplace=True, axis=1)
+    
+    #print("After",sample["Target"].value_counts())
+    
+    return sample,sample_labels
+
+if __name__ == "__main__":
+
+    # In case we need this to get mean and std from columns to normalize new data
+    #raw_data = pd.read_csv("minute_data/BTC-USD_1m.csv")
+
+    data = pd.read_csv("minute_data/BTC-USD_1M_SIGNALS.csv")
+
+    predict_data = create_predict_data(data, 30, 10, False)
+
+    data_19_20 = predict_data.loc[(predict_data["Unix"] >= 1546300800) & (
+        predict_data["Unix"] < 1609459200)]
+
+    data_2021 = predict_data.loc[predict_data["Unix"] >= 1609459200]
+
+    train_data, train_labels, test_data, test_labels = yearly_custom_splitter(
+        data_19_20, data_2021)
+
+    shape = (len(test_data.columns),)
+    
+    train_set = train_data.copy()
+    train_set["Target"] = train_labels
+    
+    #Sampling same number of elements for each class to ensure no one is more likely to be found
+    train_data,train_labels = sample_equal_target(train_set)
+    train_data.drop("Target",axis=1,inplace=True)
+    #raise Exception("Stop")
+    
+    print(train_labels.value_counts())
+    
+    if train_labels.nunique() == 2:
+        
+    
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=shape),
+            tf.keras.layers.Dense(200, activation='relu'),
+            tf.keras.layers.Dense(2000, activation='relu'),
+            tf.keras.layers.Dense(2000, activation='relu'),
+            tf.keras.layers.Dense(200, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+
+        model.compile(optimizer='adam',
+                    loss=tf.keras.losses.BinaryCrossentropy(),
+                    metrics=['accuracy', 'Precision', 'Recall', 'AUC'])
+        
+    else:
+        outputs = train_labels.nunique()
+        
+        print("Using {} classes".format(outputs))
+        
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=shape),
+            tf.keras.layers.Dense(200, activation='relu'),
+            tf.keras.layers.Dense(2000, activation='relu'),
+            tf.keras.layers.Dense(2000, activation='relu'),
+            tf.keras.layers.Dense(200, activation='relu'),
+            tf.keras.layers.Dense(outputs, activation='softmax')
+        ])
+
+        model.compile(optimizer='adam',
+                    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                    metrics=['accuracy'])
+    
+    log_dir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    #model training
+    model.fit(train_data, train_labels, epochs=10, validation_data=(test_data,test_labels),callbacks=[tensorboard_callback], batch_size=32)
+    
+    probs = model.predict(test_data)
+    
+    if outputs > 1:
+        preds = probs.argmax(axis=-1)
+    
+    
+    print(test_labels, preds)
+    print(tf.math.confusion_matrix(test_labels, preds))
+    
+    
+    #test_loss, test_acc, test_prec, test_rec, test_auc = model.evaluate(test_data,  test_labels, verbose=2)
+    
+    #time = str(datetime.date.today())
+    #model.save('./models/30_to_10m_200_2000_200_y2019_2020_'+time)
