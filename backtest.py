d22014acc274a1a8bb7bc18093c5bb0d2f666647
zefrost77 <alexis.fauxbaton@gmail.com>
2022-12-18 09:50:59 +0100

    Finished main backtest env / problem on plotting part

--- backtest.py
+++ backtest.py
@@ -6,6 +6,7 @@ import numpy as np
 from enum import Enum
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 
 class OrderType(Enum):
@@ -53,6 +54,7 @@ class Order:
 
         self.shares = quantity / entry_price
 
+        #TODO Account for leverage in position
         self.leverage = leverage
 
         self.tp = tp
@@ -74,51 +76,58 @@ class Order:
 
     def update_order(self, current_high, current_close, current_low, action):
         returns = 0
+        max_price = np.max([current_high, current_low])
+        min_price = np.min([current_high, current_low])
         if self.activity == 'conditional':
-            max_price = np.max([current_high, current_low])
-            min_price = np.min([current_high, current_low])
             if self.entry_price <= self.order_price and min_price <= self.entry_price:
-                self.activity = OrderActivity.ACTIVE
+                self.activity = OrderActivity.ACTIVE.value
 
             elif self.entry_price >= self.order_price and max_price > self.entry_price:
-                self.activity = OrderActivity.ACTIVE
+                self.activity = OrderActivity.ACTIVE.value
 
-        if action == OrderAction.NONE:
+        elif action == OrderAction.NONE.value:
+            exit_price = 0
             if self.sl != None and self.sl >= min_price:
-                self.activity = OrderActivity.CLOSED
-                if self.type == OrderType.LONG:
+                self.activity = OrderActivity.CLOSED.value
+                if self.type == OrderType.LONG.value:
                     returns = (self.sl / self.entry_price - 1) * \
                         100 * self.leverage
-                elif self.type == OrderType.SHORT:
+                    exit_price = self.sl
+                elif self.type == OrderType.SHORT.value:
                     returns = (1 - self.sl / self.entry_price) * \
                         100 * self.leverage
+                    exit_price = self.sl
 
-                print("Order {} exited at deficit, returns : {}".format(
-                    self.id, returns))
+                #print("Stop Loss hit, Order {} ({}) exited at deficit, Entry Price : {} -> {}, returns : {}".format(
+                #    self.id, self.type, self.entry_price, exit_price, returns))
                 return self.shares * self.sl
 
             if self.tp != None and self.tp <= max_price:
-                self.activity = OrderActivity.CLOSED
+                self.activity = OrderActivity.CLOSED.value
                 returns = 0
-                if self.type == OrderType.LONG:
+                if self.type == OrderType.LONG.value:
                     returns = (self.tp / self.entry_price - 1) * \
                         100 * self.leverage
-                elif self.type == OrderType.SHORT:
+                    exit_price = current_low
+                elif self.type == OrderType.SHORT.value:
                     returns = (1 - self.tp / self.entry_price) * \
                         100 * self.leverage
+                    exit_price = self.tp
 
-                print("Order {} exited at profit, returns : {}".format(
-                    self.id, returns))
+                #print("Take Profit hit, Order {} ({}) exited at profit, Entry Price : {} -> {}, returns : {}".format(
+                #    self.id, self.type, self.entry_price, exit_price, returns))
                 return self.shares * self.tp
         else:
-            if action == OrderAction.CANCEL_LONGS and self.type == OrderType.LONG:
-                self.activity = OrderActivity.CLOSED
-                returns = (self.current_close / self.entry_price -
+            if action == OrderAction.CANCEL_LONGS.value and self.type == OrderType.LONG.value:
+                self.activity = OrderActivity.CLOSED.value
+                returns = (current_close / self.entry_price -
                             1) * 100 * self.leverage
-            elif action == OrderAction.CANCEL_SHORTS and self.type == OrderType.SHORT:
-                self.activity = OrderActivity.CLOSED
-                returns = (1 - self.current_close /
+            elif action == OrderAction.CANCEL_SHORTS.value and self.type == OrderType.SHORT.value:
+                self.activity = OrderActivity.CLOSED.value
+                returns = (1 - current_close /
                             self.entry_price) * 100 * self.leverage
+            else:
+                return self.shares * current_close
 
             profit = ''
             if returns > 0:
@@ -126,8 +135,8 @@ class Order:
             else:
                 profit = 'deficit'
 
-            print("Order {} exited at {}, returns : {}".format(
-                self.id, profit, returns))
+            #print("Signal flagged, Order {} ({}) exited at {}, Entry Price : {} -> {}, returns : {}".format(
+            #    self.id, self.type, profit, self.entry_price, current_close, returns))
 
         return self.shares * current_close
 
@@ -156,14 +165,14 @@ class OrderList:
     def add_order(self, order: Order):
         self.orders.append(order)
 
-    def update_orders(self, current_high, current_low, current_close, action=OrderAction.NONE):
+    def update_orders(self, current_high, current_low, current_close, action=OrderAction.NONE.value):
         order_balance = 0
         transfer_to_balance = 0
         for order in self.orders:
             order_balance += order.update_order(current_high,
                                                 current_low, current_close, action)
 
-            if order.get_activity() == OrderActivity.CLOSED:
+            if order.get_activity() == OrderActivity.CLOSED.value:
                 transfer_to_balance += order.get_shares() * current_close
                 self.orders.remove(order)
                 
@@ -202,7 +211,7 @@ class BackTestEnv:
 
         self.start_unix = int(datetime.strptime(start, "%d/%m/%Y").timestamp())
 
-        self.end_unix = int(datetime.strptime(end, "%d/%m/%Y").timestamp())
+        self.end_unix = int(np.min([int(datetime.strptime(end, "%d/%m/%Y").timestamp()), self.datahandler.data["Unix"].max()]))
 
         self.current_unix = self.start_unix
 
@@ -222,58 +231,88 @@ class BackTestEnv:
         
         self.orders_passed = []
         
-        self.probs = self.model.predict(self.predict_data.drop(['Date', 'Unix', 'Target', 'index'], axis=1))
+        self.probs = self.model.predict(self.predict_data.drop(['Date', 'Unix', 'Target', 'index'], axis=1), batch_size=1000)
         
         self.preds = pd.Series(self.probs.argmax(axis=-1), name='Preds')
         print("Prediction type, ", type(self.preds))
-        self.preds['Unix'] = self.predict_data['Unix']
+        self.preds = pd.concat([self.preds, self.predict_data["Unix"]], axis=1)
+        #self.preds['Unix'] = self.predict_data['Unix']
         
-        self.critics = self.critic.predict(self.predict_data.drop(['Date', 'Unix', 'Target', 'index'], axis=1))
-        print(self.critics.head())
-        self.critics = pd.Series(self.critics, name='Critics')
-        self.critics['Unix'] = self.predict_data['Unix']
+        self.critics = self.critic.predict(self.predict_data.drop(['Date', 'Unix', 'Target', 'index'], axis=1), batch_size=1000)
+        print(self.critics)
+        self.critics = pd.Series(self.critics.flatten(), name='Critics')
+        self.critics = pd.concat([self.critics, self.predict_data["Unix"]], axis=1)
+        #self.critics['Unix'] = self.predict_data['Unix']
+
+        print(self.preds)
+        
+        print(self.critics)
 
     def step(self):
-        curr_data = self.datahandler.data[self.current_unix -
-                                          self.max_days+1:self.current_unix+1]
+        #curr_data = self.datahandler.data[self.current_unix -
+        #                                  self.max_days+1:self.current_unix+1]
 
         curr_data = self.datahandler.data.loc[self.datahandler.data["Unix"]
                                               == self.current_unix].reset_index(drop=True)
-
+        '''
         curr_predict_data = self.predict_data.loc[self.predict_data["Unix"]
                                                   == self.current_unix]
 
         curr_pred = self.preds.loc[self.preds["Unix"]==self.current_unix]
         
         curr_critic = self.critics.loc[self.critics["Unix"]==self.current_unix]
-
+        '''
+        
         # To drop for predictions : Date, Unix, Target
         #print(curr_predict_data.columns)
 
-        order_action = OrderAction.NONE
+        order_action = OrderAction.NONE.value
 
-        added_order = OrderType.NOTHING
+        added_order = OrderType.NOTHING.value
 
-        if len(self.order_list) == 0:
+        #if len(self.order_list) == 0:
+        if True:
+            #prob = self.model.predict(curr_predict_data.drop(
+            #    ['Date', 'Unix', 'Target', 'index'], axis=1))
 
-            prob = self.model.predict(curr_predict_data.drop(
-                ['Date', 'Unix', 'Target', 'index'], axis=1))
+            #print(self.preds.loc[self.preds["Unix"]==self.current_unix, "Preds"])
+            pred = self.preds.loc[self.preds["Unix"]==self.current_unix, "Preds"].item()
 
-            pred = prob.argmax(axis=-1)
+            #pred = prob.argmax(axis=-1)
 
-            critic = self.critic.predict(curr_predict_data.drop(
-                ['Date', 'Unix', 'Target', 'index'], axis=1))
+            #critic = self.critic.predict(curr_predict_data.drop(
+            #    ['Date', 'Unix', 'Target', 'index'], axis=1))
 
-            if critic > 0.5 and pred != OrderType.NOTHING:
-                quantity = self.balance
+            critic = self.critics.loc[self.critics["Unix"]==self.current_unix, "Critics"].item()
+
+            #print(pred, critic)
+
+            if critic > 0.5 and pred != OrderType.NOTHING.value:
+                quantity = 0.25 * self.balance
                 order_type = pred
                 order_action = order_type
+                sl = None
 
-                if self.balance >= quantity:
+                if self.balance >= quantity and quantity > 1e-6:
                     self.balance -= quantity
                     # TODO IMPLEMENT A WAY TO MANIPULATE ALREADY OPEN TRADES
-                    order = Order(type=order_type, activity=OrderActivity.ACTIVE,
-                                  order_price=curr_data.loc[0,'Close'], entry_price=curr_data.loc[0,'Close'], quantity=quantity, leverage=self.leverage)
+                    if order_type == OrderType.LONG.value:
+                        #print("LONG")
+                        sl = 0.99*(curr_data.loc[0,'Close'].item())
+                        tp = 1.03*(curr_data.loc[0,'Close'].item())
+                    elif order_type == OrderType.SHORT.value:
+                        #print("SHORT")
+                        sl = 1.01*(curr_data.loc[0,'Close'].item())
+                        tp = 0.97*(curr_data.loc[0,'Close'].item())
+                    '''
+                    print("Order Type : ", order_type)
+                    print("LONG Order : {} || SHORT Order : {}".format(OrderType.LONG.value, OrderType.SHORT.value))
+                    print("{} || {}".format((order_type == OrderType.LONG.value), (order_type == OrderType.SHORT.value)))
+                    print("Current Close : ", curr_data.loc[0,'Close'].item(), type(curr_data.loc[0,'Close'].item()))
+                    print("Stop Loss : ", sl)
+                    '''
+                    order = Order(type=order_type, activity=OrderActivity.ACTIVE.value,
+                                  order_price=curr_data.loc[0,'Close'].item(), entry_price=curr_data.loc[0,'Close'].item(), quantity=quantity, leverage=self.leverage, sl=sl, tp=tp)
                     self.order_list.add_order(order)
                     added_order = order_type
                 else:
@@ -281,7 +320,7 @@ class BackTestEnv:
 
 
         self.trading_amount, transfer_to_balance = self.order_list.update_orders(
-            curr_data['High'], curr_data['Low'], curr_data['Close'], order_action)
+            curr_data['High'].item(), curr_data['Low'].item(), curr_data['Close'].item(), order_action)
 
         self.prices.append(curr_data['Close'])
 
@@ -297,6 +336,8 @@ class BackTestEnv:
 
 
 if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    
     backtest_env = BackTestEnv("./minute_data/BTC-USD_1M_SIGNALS.csv")
 
     max_backtest_unix = backtest_env.end_unix
@@ -306,20 +347,24 @@ if __name__ == "__main__":
     n_steps = (backtest_env.end_unix - backtest_env.start_unix) / 60
 
     while backtest_env.current_unix < backtest_env.end_unix:
+        #if not (backtest_env.datahandler.data["Unix"].isin([backtest_env.current_unix]).item()):
+        if backtest_env.current_unix > backtest_env.datahandler.predict_data["Unix"].max().item():
+        #if not (backtest_env.current_unix in backtest_env.datahandler.data["Unix"].values):
+            break
         backtest_env.step()
 
-        '''
-        if count % 100 == 0:
+        if count % 1000 == 0:
+            '''
             print(backtest_env.order_list)
             print("Net Worth :", backtest_env.net_worth[-1])
             print('\n')
-        '''
-        #TODO DO PREDICTIONS ALL AT ONCE TO SAVE COMPUTE TIME
-        print("Step [{}/{}] ({} %)".format(count+1, n_steps, (count+1)/n_steps * 100), end='\r')
-        
+            '''
+            #TODO DO PREDICTIONS ALL AT ONCE TO SAVE COMPUTE TIME
+            print("Step [{}/{}] ({} %)".format(count+1, n_steps, (count+1)/n_steps * 100), end='\n')
+            
         count += 1
         
-    N = len(backtest_env.prices)
+    N = pd.Series(range(0,len(backtest_env.prices)))
     
     prices = pd.Series(backtest_env.prices)
     
@@ -331,9 +376,10 @@ if __name__ == "__main__":
     
     axis.plot(N, prices, label='Price')
     
-    axis.scatter(N, prices[orders_passed == OrderType.LONG], 'o', color='b', label='Long')
+    #TODO FIX N size and prices[...] size that must be the same
+    axis.scatter(N[orders_passed == OrderType.LONG.value], prices[orders_passed == OrderType.LONG.value], 'o', color='b', label='Long')
     
-    axis.scatter(N, prices[orders_passed == OrderType.SHORT], 'o', color='r', label='Short')
+    axis.scatter(N[orders_passed == OrderType.SHORT.value], prices[orders_passed == OrderType.SHORT.value], 'o', color='r', label='Short')
     
     net_worth_axis = axis.twinx()
     
